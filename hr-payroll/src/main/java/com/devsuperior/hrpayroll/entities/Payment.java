3bcd428b044a0c1242af0100ac713bd78fc3a799
Januario <januario86fp@gmail.com>
2024-05-28 20:51:34 -0300

    fix: update entity payment

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/entities/Payment.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/entities/Payment.java
@@ -1,7 +1,11 @@
 package com.devsuperior.hrpayroll.entities;
 
+import java.io.Serializable;
+
 public class Payment implements Serializable{
 
+	private static final long serialVersionUID = -7674964142288409514L;
+	
 	private String name;
 	private Double dailyIncome;
 	private Integer days;
